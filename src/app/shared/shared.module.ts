54749f06eb93c97228af42a2eff176cd2eef50ed
Adam Nowak <adamus79@gmail.com>
2022-11-11 00:34:41 +0100

    back to peaceiris/actions-gh-pages@v3

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -103,7 +103,7 @@ const materialModules = [
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient, "~/assets/i18n/", ".json");
+  return new TranslateHttpLoader(httpClient, "/assets/i18n/", ".json");
 }
 
 @NgModule({
